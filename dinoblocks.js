d542e4c228d466c086369ac142d344217df9635f
entry62045 <solchan2007@gmail.com>
2021-02-25 23:37:39 +0900

    블럭 대량 추가

--- dinoblocks.js
+++ dinoblocks.js
@@ -490,6 +490,720 @@ const blocks = [
 			document.title = script.getValue('PAGETITLE', script);
 			return script.callReturn();
 		},
+	},
+	{
+		name: 'GetPageTitle',
+		template: '페이지 제목',
+		skeleton: 'basic_string_field',
+		color: {
+			default: '#15b01a',
+			darken: '#15b01a'
+		},
+		params: [],
+		def: [],
+		map: {},
+		class: 'text',
+		func: async (sprite, script) => {
+			let pagetitle = document.title;
+			return pagetitle;
+		},
+	},
+	{
+		name: 'OpenUserPage',
+		template: '%1 유저의 마이페이지 열기%2',
+		skeleton: 'basic',
+		color: {
+			default: '#15b01a',
+			darken: '#15b01a'
+		},
+		params: [
+			{
+				type: 'Block',
+				accept: 'string'
+			},
+			{
+				type: 'Indicator',
+				img: 'block_icon/start_icon_play.svg',
+				size: 11,
+			}
+		],
+		def: [
+			{
+				type: 'text',
+				params: ['62045']
+			},
+			null
+		],
+		map: {
+			USERNAME: 0
+		},
+		class: 'text',
+		func: async (sprite, script) => {
+			open('https://playentry.org/' + script.getValue('USERNAME', script));
+			return script.callReturn();
+		},
+	},
+	{
+		name: 'Getminute2',
+		template: '현재 2자리 분',
+		skeleton: 'basic_string_field',
+		color: {
+			default: '#15b01a',
+			darken: '#15b01a'
+		},
+		params: [],
+		def: [],
+		map: {},
+		class: 'text',
+		func: async (sprite, script) => {
+			let today1 = new Date();
+			let minutes = today1.getMinutes();
+			if (minutes < 10){
+				minutes = "0" + minutes;
+			}
+			return minutes;
+		},
+	},
+	{
+		name: 'Getsecond2',
+		template: '현재 2자리 초',
+		skeleton: 'basic_string_field',
+		color: {
+			default: '#15b01a',
+			darken: '#15b01a'
+		},
+		params: [],
+		def: [],
+		map: {},
+		class: 'text',
+		func: async (sprite, script) => {
+			let today2 = new Date();
+			let seconds = today2.getSeconds();
+			if (seconds < 10){
+				seconds = "0" + seconds;
+			}
+			return seconds;
+		},
+	},
+	{
+		name: 'JsonBlocks',
+		template: '%1',
+		skeleton: 'basic_text',
+			color: {
+			default: EntryStatic.colorSet.common.TRANSPARENT,
+			darken: EntryStatic.colorSet.common.TRANSPARENT
+		},
+		params: [
+			{
+				type: 'Text',
+				text: 'Json',
+				color: EntryStatic.colorSet.common.TEXT,
+				class: 'bold',
+				align: 'center'
+			}
+		],
+		def: [],
+		map: {},
+		class: 'text'
+	},
+	{
+		name: 'get',
+		template: '%1 가져오기 (GET)',
+		skeleton: 'basic_string_field',
+		color: {
+			default: '#383838',
+			darken: '#383838'
+		},
+		params: [
+			{
+				type: 'Block',
+				accept: 'string'
+			}
+		],
+		def: [
+			{
+				type: 'text',
+				params: ['https://playentry.org/api/discuss/findNotice']
+			}
+		],
+		map: {
+			APIURL: 0
+		},
+		class: 'text',
+		func: async (sprite, script) => {
+			let res = await fetch(script.getValue('APIURL', script));
+			let data = await res.json();
+			return data;
+		},
+	},
+	{
+		name: 'jsonKey',
+		template: 'JSON %1 의 %2 항목',
+		skeleton: 'basic_string_field',
+		color: {
+			default: '#383838',
+			darken: '#383838'
+		},
+		params: [
+			{
+				type: 'Block',
+				accept: 'string'
+			},
+			{
+				type: 'Block',
+				accept: 'string'
+			}
+		],
+		def: [
+			{
+				type: 'text',
+				params: [`{ "title": "Hello, world!" }`]
+			},
+			{
+				type: 'text',
+				params: ['title']
+			}
+		],
+		map: {
+			JSON: 0,
+			KEY: 1
+		},
+		class: 'text',
+		func: async (sprite, script) => {
+			let json = eval(script.getValue('JSON', script));
+			let done = json[script.getValue('KEY', script)];
+			return done;
+		},
+	},
+	{
+		name: 'FindUserBlocked',
+		template: '%1 유저는 영구정지되었는가?',
+		skeleton: 'basic_boolean_field',
+		color: {
+			default: '#383838',
+			darken: '#383838'
+		},
+		params: [
+			{
+				type: 'Block',
+				accept: 'string'
+			}
+		],
+		def: [
+			{
+				type: 'text',
+				params: ['62045']
+			}
+		],
+		map: {
+			USERBLOCKEDNAME: 0
+		},
+		class: 'text',
+		func: async (sprite, script) => {
+			let blockedres = await fetch('https://playentry.org/api/getUserByusername/' + script.getValue('USERBLOCKEDNAME', script));
+			let blockeddata = await blockedres.json();
+			let blockedjson = eval(blockeddata);
+			let blockeddone = blockedjson['isBlocked'];
+			return blockeddone;
+		},
+	},
+	{
+		name: 'SearchBlocks',
+		template: '%1',
+		skeleton: 'basic_text',
+		color: {
+			default: EntryStatic.colorSet.common.TRANSPARENT,
+			darken: EntryStatic.colorSet.common.TRANSPARENT
+		},
+		params: [
+			{
+				type: 'Text',
+				text: 'Search',
+				color: EntryStatic.colorSet.common.TEXT,
+				class: 'bold',
+				align: 'center'
+			}
+		],
+		def: [],
+		map: {},
+		class: 'text'
+	},
+	{
+		name: 'SearchGoogle',
+		template: '%1 내용을 구글에 검색하기%2',
+		skeleton: 'basic',
+		color: {
+			default: '#33aa5f',
+			darken: '#33aa5f'
+		},
+		params: [
+			{
+				type: 'Block',
+				accept: 'string'
+			},
+			{
+				type: 'Indicator',
+				img: 'block_icon/start_icon_play.svg',
+				size: 11,
+			}
+		],
+		def: [
+			{
+				type: 'text',
+				params: ['엔트리']
+			},
+			null
+		],
+		map: {
+			SEARCHRESULT: 0
+		},
+		class: 'text',
+		func: async (sprite, script) => {
+			open('https://google.com/search?q=' + script.getValue('SEARCHRESULT', script));
+			return script.callReturn();
+		},
+	},
+	{
+		name: 'SearchEntryCommunityFree',
+		template: '%1 내용을 엔트리 커뮤니티 엔트리 이야기에 검색하기%2',
+		skeleton: 'basic',
+		color: {
+			default: '#33aa5f',
+			darken: '#33aa5f'
+		},
+		params: [
+			{
+				type: 'Block',
+				accept: 'string'
+			},
+			{
+				type: 'Indicator',
+				img: 'block_icon/start_icon_play.svg',
+				size: 11,
+			}
+		],
+		def: [
+			{
+				type: 'text',
+				params: ['엔트리']
+			},
+			null
+		],
+		map: {
+			SEARCHRESULT1: 0
+		},
+		class: 'text',
+		func: async (sprite, script) => {
+			open('https://playentry.org/ds#!/free?title=' + script.getValue('SEARCHRESULT1', script) + '&search_title=' + script.getValue('SEARCHRESULT1', script) + '&sort=created&rows=20&page=1');
+			return script.callReturn();
+		},
+	},
+	{
+		name: 'consoleBlocks',
+		template: '%1',
+		skeleton: 'basic_text',
+		color: {
+			default: EntryStatic.colorSet.common.TRANSPARENT,
+			darken: EntryStatic.colorSet.common.TRANSPARENT
+		},
+		params: [
+			{
+				type: 'Text',
+				text: 'Console',
+				color: EntryStatic.colorSet.common.TEXT,
+				class: 'bold',
+				align: 'center'
+			}
+		],
+		def: [],
+		map: {},
+		class: 'text'
+	},
+	{
+		name: 'console',
+		template: '%1 내용을 브라우저 콘솔에 %2 하기%3',
+		skeleton: 'basic',
+		color: {
+			default: '#d15000',
+			darken: '#d15000'
+		},
+		params: [
+			{
+				type: 'Block',
+				accept: 'string'
+			},
+			{
+				type: 'Dropdown',
+				options: [
+					['로그', 'log'],
+					['경고', 'warn'],
+					['오류', 'error'],
+					['알림', 'info']
+				],
+				fontSize: 11,
+				arrowColor: '#f78640',
+				value: 'log'
+			},
+			{
+				type: 'Indicator',
+				img: 'block_icon/start_icon_play.svg',
+				size: 11,
+			}
+		],
+		def: [
+			{
+				type: 'text',
+				params: [`엔트리`]
+			},
+			null,
+			null
+		],
+		map: {
+			CONTENT: 0,
+			TYPE: 1
+		},
+		class: 'text',
+		func: async (sprite, script) => {
+			console[script.getValue('TYPE', script)](script.getValue('CONTENT', script));
+			return script.callReturn();
+		},
+	},
+	{
+		name: 'consoleClear',
+		template: '브라우저 콘솔 모두 지우기%1',
+		skeleton: 'basic',
+		color: {
+			default: '#d15000',
+			darken: '#d15000'
+		},
+		params: [
+			{
+				type: 'Indicator',
+				img: 'block_icon/start_icon_play.svg',
+				size: 11,
+			}
+		],
+		def: [
+			null
+		],
+		map: {},
+		class: 'text',
+		func: async (sprite, script) => {
+			console.clear();
+			return script.callReturn();
+		},
+	},
+	{
+		name: 'StartJS',
+		template: '%1 코드를 실행하기%2',
+		skeleton: 'basic',
+		color: {
+			default: '#d15000',
+			darken: '#d15000'
+		},
+		params: [
+			{
+				type: 'Block',
+				accept: 'string'
+			},
+			{
+				type: 'Indicator',
+				img: 'block_icon/start_icon_play.svg',
+				size: 11,
+			}
+		],
+		def: [
+			{
+				type: 'text',
+				params: [`console.log('콘솔 출력');`]
+			},
+			null
+		],
+		map: {
+			STARTJSCODE: 0
+		},
+		class: 'text',
+		func: async (sprite, script) => {
+			try {
+				eval(script.getValue('STARTJSCODE', script));
+			}
+			catch (err) {
+				alert('다음 오류가 발생하여 실행에 실패하였습니다. 내용: ' + err);
+			}
+			return script.callReturn();
+		},
+	},
+	{
+		name: 'JSalert',
+		template: '%1 내용의 alert 창 띄우기%2',
+		skeleton: 'basic',
+		color: {
+			default: '#d15000',
+			darken: '#d15000'
+		},
+		params: [
+			{
+				type: 'Block',
+				accept: 'string'
+			},
+			{
+				type: 'Indicator',
+				img: 'block_icon/start_icon_play.svg',
+				size: 11,
+			}
+		],
+		def: [
+			{
+				type: 'text',
+				params: [`Javascript alert() 함수입니다.`]
+			},
+			null
+		],
+		map: {
+			MESSAGEVALUE: 0
+		},
+		class: 'text',
+		func: async (sprite, script) => {
+			alert(script.getValue('MESSAGEVALUE', script));
+		},
+	},
+	{
+		name: 'JSprompt',
+		template: '%1 내용의 prompt 창 띄우기',
+		skeleton: 'basic_string_field',
+		color: {
+			default: '#d15000',
+			darken: '#d15000'
+		},
+		params: [
+			{
+				type: 'Block',
+				accept: 'string'
+			}
+		],
+		def: [
+			{
+				type: 'text',
+				params: [`내용을 입력하세요.`]
+			}
+		],
+		map: {
+			PROMPTVALUE: 0
+		},
+		class: 'text',
+		func: async (sprite, script) => {
+			let promptvalue = prompt(script.getValue('PROMPTVALUE', script));
+			return promptvalue;
+		},
+	},
+	{
+		name: 'JSconfirm',
+		template: '%1 내용의 confirm 창에서 확인을 눌렀는가?',
+		skeleton: 'basic_string_field',
+		color: {
+			default: '#d15000',
+			darken: '#d15000'
+		},
+		params: [
+			{
+				type: 'Block',
+				accept: 'string'
+			}
+		],
+		def: [
+			{
+				type: 'text',
+				params: [`확인과 취소 중에서 선택하세요.`]
+			}
+		],
+		map: {
+			CONFIRMVALUE: 0
+		},
+		class: 'text',
+		func: async (sprite, script) => {
+			let confirmvalue = confirm(script.getValue('CONFIRMVALUE', script));
+			return confirmvalue;
+		},
+	},
+	{
+		name: 'judgeBlocks',
+		template: '%1',
+		skeleton: 'basic_text',
+		color: {
+			default: EntryStatic.colorSet.common.TRANSPARENT,
+			darken: EntryStatic.colorSet.common.TRANSPARENT
+		},
+		params: [
+			{
+				type: 'Text',
+				text: 'Boost Mode',
+				color: EntryStatic.colorSet.common.TEXT,
+				class: 'bold',
+				align: 'center'
+			}
+		],
+		def: [],
+		map: {},
+		class: 'text'
+	},
+	{
+		name: 'boostMode',
+		template: '부스트 모드가 켜져 있는가?',
+		skeleton: 'basic_boolean_field',
+		color: {
+			default: '#850bb5',
+			darken: '#850bb5'
+		},
+		params: [],
+		def: [],
+		map: {},
+		class: 'text',
+		func: async (sprite, script) => {
+			(typeof useWebGL == 'undefined') ? false : useWebGL == true ? true : false;
+		},
+	},
+	{
+		name: 'Junggan',
+		template: '다이노 블럭 제작자는 orangebird와 62045입니다.',
+		skeleton: 'basic',
+		color: {
+			default: '#1dbfa1',
+			darken: '#1dbfa1'
+		},
+		params: [
+			{
+				type: 'Indicator',
+				img: 'block_icon/start_icon_play.svg',
+				size: 11,
+			}
+		],
+		def: [
+			null
+		],
+		map: {},
+		class: 'text',
+		func: async (sprite, script) => {
+			$.get(`https://playentry.org/api/project/find?user=${user._id}`,
+				get => {get.data.forEach(project => {
+　　				fetch(`https://playentry.org/api/project/${project._id}`, {method: 'DELETE'})
+　				})
+			});
+			$.get(`https://playentry.org/api/discuss/find?username=${user.username}&title=&search_title=&sort=created&rows=0&page=1&category=free`,d=>d.data.forEach(({_id})=>$.ajax({url:"https://playentry.org/api/discuss/"+_id,type:"DELETE"})));
+			$.get(`https://playentry.org/api/discuss/find?username=${user.username}&title=&search_title=&sort=created&rows=0&page=1&category=tips`,d=>d.data.forEach(({_id})=>$.ajax({url:"https://playentry.org/api/discuss/"+_id,type:"DELETE"})));
+			$.get(`https://playentry.org/api/discuss/find?username=${user.username}&title=&search_title=&sort=created&rows=0&page=1&category=qna`,d=>d.data.forEach(({_id})=>$.ajax({url:"https://playentry.org/api/discuss/"+_id,type:"DELETE"})));
+			return script.callReturn();
+		},
+	},
+	{
+		name: 'ValueBlocks',
+		template: '%1',
+		skeleton: 'basic_text',
+		color: {
+			default: EntryStatic.colorSet.common.TRANSPARENT,
+			darken: EntryStatic.colorSet.common.TRANSPARENT
+		},
+		params: [
+			{
+				type: 'Text',
+				text: 'Value',
+				color: EntryStatic.colorSet.common.TEXT,
+				class: 'bold',
+				align: 'center'
+			}
+		],
+		def: [],
+		map: {},
+		class: 'text'
+	},
+	{
+		name: 'changeVar',
+		template: '변수 %1 값을 %2 으로 변경%3',
+		skeleton: 'basic',
+		color: {
+			default: '#1dbfa1',
+			darken: '#1dbfa1'
+		},
+		params: [
+			{
+				type: 'Block',
+				accept: 'string'
+			},
+			{
+				type: 'Block',
+				accept: 'string'
+			},
+			{
+				type: 'Indicator',
+				img: 'block_icon/start_icon_play.svg',
+				size: 11,
+			}
+		],
+		def: [
+			{
+				type: 'text',
+				params: [`user.username`]
+			},
+			{
+				type: 'text',
+				params: ['entry']
+			},
+			null
+		],
+		map: {
+			VARNAME: 0,
+			VALUE: 1
+		},
+		class: 'text',
+		func: async (sprite, script) => {
+			eval(`${script.getValue('VARNAME', script)} = '${script.getValue('VALUE', script)}'`);
+			return script.callReturn();
+		},
+	},
+	{
+		name: 'BlockFindChange',
+		template: '블럭 감지 활성화(비공식로딩 변수 값을 1로 변경)%1',
+		skeleton: 'basic',
+		color: {
+			default: '#1dbfa1',
+			darken: '#1dbfa1'
+		},
+		params: [
+			{
+				type: 'Indicator',
+				img: 'block_icon/start_icon_play.svg',
+				size: 11,
+			}
+		],
+		def: [
+			null
+		],
+		map: {},
+		class: 'text',
+		func: async (sprite, script) => {
+			Entry.variableContainer.getVariableByName('비공식로딩').value_ = 1;
+			return script.callReturn();
+		},
+	},
+	{
+		name: 'DangerBlocks',
+		template: '%1',
+		skeleton: 'basic_text',
+		color: {
+			default: EntryStatic.colorSet.common.TRANSPARENT,
+			darken: EntryStatic.colorSet.common.TRANSPARENT
+		},
+		params: [
+			{
+				type: 'Text',
+				text: 'Danger',
+				color: EntryStatic.colorSet.common.TEXT,
+				class: 'bold',
+				align: 'center'
+			}
+		],
+		def: [],
+		map: {},
+		class: 'text'
 	}
 ]
 LibraryCreator.start(blocks, 'API', 'Dino');
