fbd02325b7877e04d8b6756c32a9f220177c7238
62045 <77736636+entry62045@users.noreply.github.com>
2022-02-08 18:34:04 +0900

    Update dinoblocks.js

--- dinoblocks.js
+++ dinoblocks.js
@@ -2126,4 +2126,149 @@ async function DinoBlockLoad() {
 	}
     console.log("DinoBlocks v1.2")
 }
-DinoBlockLoad();
+async function DinoBlockLoadNew() {
+	if(Entry.getMainWS() && Entry.projectId) {
+		const TempProjectId = Entry.projectId;
+		const ExportedProject = Entry.exportProject();
+		const ProjectData = (await (await await fetch('https://playentry.org/graphql', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json'
+			},
+			body: JSON.stringify({
+				query:
+				` query SELECT_PROJECT($id: ID! $groupId: ID) {
+					project(id: $id, groupId: $groupId) {
+						id
+						name
+						user {
+							id
+							nickname
+							username
+							profileImage {
+								id
+								name
+								label {
+									ko
+									en
+									ja
+									vn
+								}
+								filename
+								imageType
+								dimension {
+									width
+									height
+								}
+								trimmed {
+									filename
+									width
+									height
+								}
+							}
+							status {
+								following
+								follower
+							}
+							description
+							role
+						}
+						visit
+						speed
+						objects
+						variables
+						cloudVariable
+						messages
+						functions
+						tables
+						scenes
+						thumb
+						isopen
+						blamed
+						isPracticalCourse
+						category
+						categoryCode
+						created
+						updated
+						shortenUrl
+						parent {
+							id
+							name
+							user {
+								id
+								username
+								nickname
+							}
+						}
+						likeCnt
+						favorite
+						special
+						isForLecture
+						isForStudy
+						isForSubmit
+						hashId
+						complexity
+						staffPicked
+						ranked
+						submitId {
+							id
+						}
+						description
+						description2
+						description3
+						hasRealTimeVariable
+						realTimeVariable {
+							variableType
+							key
+							value
+							array {
+								key
+								data
+							}
+							minValue
+							maxValue
+							visible
+							x
+							y
+							width
+							height
+							object
+						}
+						commentGroup {
+							group
+							count
+						}
+						likeCntGroup {
+							group
+							count
+						}
+						visitGroup {
+							group
+							count
+						}
+						recentGroup {
+							group
+							count
+						}
+						learning
+						expansionBlocks
+						aiUtilizeBlocks
+						childCnt
+					}
+				}
+				`,
+				variables: { id: TempProjectId }
+			})
+		})).json()).data.project;
+		Entry.clearProject();
+		Entry.loadProject(Object.keys(ExportedProject).reduce((acc, cur) => {
+			acc[cur] = ProjectData[cur];
+			return acc;
+		}, {}));
+		Entry.projectId = TempProjectId;
+	}
+    console.log("DinoBlocks v1.2")
+}
+// DinoBlockLoad();
+// 리뉴얼 이후 API 변경으로 새 코드로 교체
+DinoBlockLoadNew();
